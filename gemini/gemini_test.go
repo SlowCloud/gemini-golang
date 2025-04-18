bb3d4d7bfab4e7f1433cad9f97870919b77a2f5e
slowcloud <sju9417@gmail.com>
2025-04-18 22:09:10 +0900

    [refactor] 테스트 코드 리팩토링

--- gemini/gemini_test.go
+++ gemini/gemini_test.go
@@ -8,6 +8,32 @@ import (
 	"github.com/SlowCloud/gemini-golang/gemini"
 )
 
+func TestGeminiCreate(t *testing.T) {
+	gemini := createGemini()
+	if gemini == nil {
+		t.FailNow()
+	}
+}
+
+func TestGeminiAsk(t *testing.T) {
+	skipShort(t)
+	gemini := createGemini()
+	msg, err := gemini.Ask("hello!")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(msg) == 0 {
+		t.Fail()
+	}
+	fmt.Println("ask: hello!")
+	fmt.Println("answer: " + msg)
+}
+
+func TestCreateChatSession(t *testing.T) {
+	gemini := createGemini()
+	createChatSession(gemini)
+}
+
 func createGemini() *gemini.Gemini {
 	gemini, err := gemini.New()
 	if err != nil {
@@ -22,31 +48,10 @@ func skipShort(t *testing.T) {
 	}
 }
 
-func TestGeminiCreate(t *testing.T) {
-	gemini := createGemini()
-	if gemini == nil {
-		t.FailNow()
-	}
-}
-
-func TestGeminiAsk(t *testing.T) {
-	skipShort(t)
-	gemini := createGemini()
-	msg, err := gemini.Ask("hello!")
+func createChatSession(gemini *gemini.Gemini) *gemini.ChatSession {
+	chat, err := gemini.CreateChat()
 	if err != nil {
 		log.Fatal(err)
 	}
-	if len(msg) == 0 {
-		t.Fail()
-	}
-	fmt.Println("ask: hello!")
-	fmt.Println("answer: " + msg)
-}
-
-func TestCreateChatSession(t *testing.T) {
-	gemini := createGemini()
-	chat, err := gemini.CreateChat()
-	if err != nil || chat == nil {
-		t.Fail()
-	}
+	return chat
 }
