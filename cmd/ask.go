feb64d078c115bbc02cef9b2bf7b8dda160a600a
slowcloud <sju9417@gmail.com>
2025-04-24 16:16:17 +0900

    [remove] `ask`의 불필요한 기능 및 옵션 제거(#8)

--- cmd/ask.go
+++ cmd/ask.go
@@ -4,19 +4,12 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"bufio"
 	"fmt"
 	"log"
-	"os"
 	"strings"
 
 	"github.com/SlowCloud/gemini-golang/gemini"
 	"github.com/spf13/cobra"
-)
-
-var (
-	geminiModel *gemini.Gemini
-	longFlag    *bool
 )
 
 var askCmd = &cobra.Command{
@@ -25,35 +18,22 @@ var askCmd = &cobra.Command{
 	Long: `간단한 1줄 질문을 수행할 수 있습니다.
 옵션을 통해 이미지나 PDF를 등록하는 등의 작업을 추가로 수행할 수 있습니다.(예정)`,
 	Run: func(cmd *cobra.Command, args []string) {
-
-		var words string
-
-		if *longFlag {
-			scanner := bufio.NewScanner(os.Stdin)
-			for scanner.Scan() {
-				words += scanner.Text()
-			}
-		} else {
-			words = strings.Join(args, " ")
-		}
-
-		answer, err := geminiModel.Ask(words)
+		gemini, err := gemini.New()
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		words := strings.Join(args, " ")
+
+		answer, err := gemini.Ask(words)
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		fmt.Println(answer)
 	},
 }
 
 func init() {
-
-	var err error
-	geminiModel, err = gemini.New()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	longFlag = askCmd.Flags().BoolP("long", "l", false, "긴 입력이 필요하다면, 해당 옵션을 입력해주세요. 명령어 인자로 들어간 입력은 무시됩니다.")
-
 	rootCmd.AddCommand(askCmd)
 }
