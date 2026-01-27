1405929b6a729baef5d7ccb78d12694b8567d033
slowcloud <sju9417@gmail.com>
2026-01-27 13:02:26 +0900

    최소 기능으로 재구현

--- config/config.go
+++ config/config.go
@@ -2,6 +2,5 @@ package configuration
 
 var (
 	TextareaSize                    = 5
-	DefaultApiKeyEnviromentVariable = "GEMINI_API_KEY"
-	DefaultGeminiModel              = "gemini-2.0-flash-exp"
+	DefaultApiKeyEnviromentVariable = "GOOGLE_API_KEY"
 )
--- main.go
+++ main.go
@@ -4,22 +4,66 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-	"github.com/SlowCloud/gemini-golang/gemini"
-	"github.com/SlowCloud/gemini-golang/ui"
-	tea "github.com/charmbracelet/bubbletea"
+	configuration "github.com/SlowCloud/gemini-golang/config"
+	"google.golang.org/genai"
 )
 
 func main() {
-	gemini, err := gemini.New()
-	if err != nil {
-		log.Fatal(err)
-	}
-	model := ui.New(gemini)
 
-	p := tea.NewProgram(model, tea.WithAltScreen())
-	if _, err := p.Run(); err != nil {
-		log.Fatal(err)
+	background := context.Background()
+
+	ctx, cancel := context.WithTimeout(background, 5*time.Second)
+	client, err := genai.NewClient(ctx, &genai.ClientConfig{APIKey: os.Getenv(configuration.DefaultApiKeyEnviromentVariable)})
+	if err != nil {
+		panic(err)
 	}
+	defer cancel()
+
+	ctx, cancel = context.WithTimeout(background, 5*time.Second)
+	chat, err := client.Chats.Create(ctx, "gemini-2.5-flash", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer cancel()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigChan
+		fmt.Println()
+		fmt.Println("채팅을 종료합니다.")
+		os.Exit(0)
+	}()
+
+	for {
+
+		var s string
+		fmt.Print("> ")
+		fmt.Scan(&s)
+
+		ctx, cancel = context.WithTimeout(background, 1*time.Minute)
+		iter := chat.SendMessageStream(ctx, genai.Part{Text: s})
+
+		for tok, err := range iter {
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Print(tok.Text())
+		}
+
+		cancel()
+
+		fmt.Println()
+
+	}
+
 }
