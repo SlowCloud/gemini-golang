79299b9d374b8af801ea365544df53516fcd04da
slowcloud <sju9417@gmail.com>
2026-02-01 13:32:50 +0900

    goChatUsecase의 context를 TODO로 변경

--- core/goChatUsecase.go
+++ core/goChatUsecase.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"context"
+	"log"
 	"os"
 	"time"
 
@@ -19,6 +20,7 @@ func NewGoChatUsecase() ChatUsecase {
 	ctx, cancel := context.WithTimeout(background, 10*time.Second)
 	client, err := genai.NewClient(ctx, &genai.ClientConfig{APIKey: os.Getenv(configuration.DefaultApiKeyEnviromentVariable)})
 	if err != nil {
+		log.Fatal("클라이언트 생성 실패")
 		panic(err)
 	}
 	defer cancel()
@@ -26,6 +28,7 @@ func NewGoChatUsecase() ChatUsecase {
 	ctx, cancel = context.WithTimeout(background, 10*time.Second)
 	chat, err := client.Chats.Create(ctx, "gemini-2.5-flash", nil, nil)
 	if err != nil {
+		log.Fatal("채팅 세션 생성 실패")
 		panic(err)
 	}
 	defer cancel()
@@ -39,17 +42,19 @@ func (g goChatUsecase) Chat(text string) string {
 
 // chatStream implements ChatUsecase.
 func (g goChatUsecase) ChatStream(text string) (<-chan string, error) {
-	background := context.Background()
-	ctx, cancel := context.WithTimeout(background, 1*time.Minute)
-	iter := g.chat.SendMessageStream(ctx, genai.Part{Text: text})
-	defer cancel()
+	// background := context.Background()
+	// ctx, cancel := context.WithTimeout(background, 1*time.Minute)
+	iter := g.chat.SendMessageStream(context.TODO(), genai.Part{Text: text})
+	// defer cancel()
 
 	outputChan := make(chan string)
 	go func() {
 		defer close(outputChan)
+		log.Println("채팅 받는 중...")
 		for tok := range iter {
 			outputChan <- tok.Text()
 		}
+		log.Println("채팅 완료.")
 	}()
 
 	return outputChan, nil
