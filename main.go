a629771901985c60e250c1bed837579baf5b0b11
slowcloud <sju9417@gmail.com>
2026-02-07 12:24:30 +0900

    히스토리 저장 기능을 함수로 추출

--- main.go
+++ main.go
@@ -78,10 +78,18 @@ func chat() {
 		fmt.Println()
 	}
 
+	err := saveHistory(goChat)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func saveHistory(goChat core.ChatUsecase) error {
 	history, err := goChat.GetHistory()
 	if err != nil {
 		fmt.Println("Error getting history:", err)
-		return
+		return err
 	}
 
 	now := time.Now().Local().Format("2006-01-02_150405")
@@ -90,14 +98,14 @@ func chat() {
 	dir, err := os.Getwd()
 	if err != nil {
 		fmt.Println("Error getting working directory:", err)
-		return
+		return err
 	}
 	err = os.WriteFile(filepath.Join(dir, filename), history, 0644)
 	if err != nil {
 		fmt.Println("Error writing history to file:", err)
-		return
+		return err
 	}
 
 	fmt.Println("Chat history saved to", filename, "path ", dir)
-
+	return nil
 }
